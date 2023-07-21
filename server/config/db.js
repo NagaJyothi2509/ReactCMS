7d6d626adcd621e06a0100e580dca7ec5d248293
NagaJyothi2509 <64900359+NagaJyothi2509@users.noreply.github.com>
2023-07-21 12:20:14 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 const connectDB=async()=>{
     return mongoose
-    .connect("mongodb+srv://admin:<password>@jyothiscluster.oyazb86.mongodb.net/contact_mern?retryWrites=true&w=majority")
+    .connect("mongodb+srv://admin:admin@jyothiscluster.oyazb86.mongodb.net/contact_mern?retryWrites=true&w=majority")
     .then(()=>console.log("database connected succesfully"))
     .catch((err)=>console.log(err));
 };
